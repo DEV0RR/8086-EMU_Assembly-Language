4ecf1d3208e9ed7b1b5806477e02f78074f88a45
Diba_Dev_PCIU <156682404+DEV0RR@users.noreply.github.com>
2024-12-24 19:47:57 +0600

    Create finalreport2.asm

---
+++ Lab_Report/finalreport2.asm
@@ -0,0 +1,12 @@
+F 6 (9 tay delivery dite hobe) 
+
+Chicken sub with cheese 1 ta 
+Maxican chicken pasta 1 ta 
+Sweet lacchi large  1 ta
+
+address: Nalapara moshjid er pasher building, 7th floor 
+01767839467
+
+Fb 350
+Dc 50
+Tb 400
